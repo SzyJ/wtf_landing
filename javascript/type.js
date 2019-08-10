81d0a64019fc8aa18ba0f91baa76eee704d51817
Szymon <szy.jackiewicz@gmail.com>
2019-08-10 14:34:05 +0100

    Add typing effect

---
+++ javascript/type.js
@@ -0,0 +1,29 @@
+var i = 0;
+var txt = 'sZY.WTF';
+var speed = 50;
+
+window.onload = function() {
+	var titleDiv = document.getElementById("title_foreground");
+	titleDiv.innerHTML = '|' + '&nbsp;'.repeat(txt.length - 1);
+	
+	const DELAY = 1000;
+	
+	setTimeout(typeTitle, DELAY);
+}
+
+
+function typeTitle() {
+	var foregroundElement = document.getElementById("title_foreground");
+	
+	if (i < txt.length) {
+		var current = foregroundElement.innerHTML;
+		
+		foregroundElement.innerHTML = current.substr(0, i) + txt.charAt(i) 
+		if (i < txt.length - 1) {
+			foregroundElement.innerHTML += '|' + '&nbsp;'.repeat(txt.length - i - 2);
+		}
+		
+		i++;
+		setTimeout(typeTitle, speed);
+	}
+}
